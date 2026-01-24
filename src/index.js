2c19e0613302b4d6b3f77432d064d13e34f91a6e
Maximilien Ledoux <242964282+max-ldx@users.noreply.github.com>
2026-01-24 20:33:11 +0100

    feat: add basic weather service

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//import { WeatherService } from "./weather-service";
+
+//const weatherService = new WeatherService();
+//const data = await weatherService.fetchData({ location: "Thimister" });
+//console.log(data);
