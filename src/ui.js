d8f0d2e95faff7e5e1982c6a18e3cd2cfd1d8065
Maximilien Ledoux <242964282+max-ldx@users.noreply.github.com>
2026-02-08 17:45:49 +0100

    feat: display current conditions

--- src/ui.js
+++ src/ui.js
@@ -6,6 +6,20 @@ export function initUI(weatherService) {
 		const formData = new FormData(form);
 		const search = formData.get("search");
 		const data = await weatherService.fetchData({ location: search });
-		console.log(data);
+		render(data);
 	});
+}
+
+function render(data) {
+	const address = document.querySelector("#address");
+    const temp = document.querySelector("#temp");
+    const feelslike = document.querySelector("#feelslike");
+    const tempmin = document.querySelector("#tempmin");
+    const tempmax = document.querySelector("#tempmax");
+
+	address.textContent = data.address;
+    temp.textContent = `${data.temp} °C`;
+    feelslike.textContent = `Feels like: ${data.feelslike} °C`;
+    tempmin.textContent = `${data.tempmin} °C`;
+    tempmax.textContent = `${data.tempmax} °C`;
 }
