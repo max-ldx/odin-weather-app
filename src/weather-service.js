507b5bc8b204feb6435e598d7169985084cce0a6
Maximilien Ledoux <242964282+max-ldx@users.noreply.github.com>
2026-02-08 17:19:22 +0100

    feat: add data filter

--- src/weather-service.js
+++ src/weather-service.js
@@ -4,13 +4,12 @@ export class WeatherService {
 	async fetchData({ location = "", system = "metric" } = {}) {
 		try {
 			const response = await fetch(
-				`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${system}&key=${this.#api_key}&contentType=json`,
+				`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next24hours?unitGroup=${system}&key=${this.#api_key}&contentType=json`,
 			);
 			if (!response.ok) {
 				throw new Error("Failed to fetch data from weather API");
 			}
 			const data = await response.json();
-			// TODO: filter data
 			return data;
 		} catch (error) {
 			console.log(error);
